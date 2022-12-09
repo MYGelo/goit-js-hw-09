f53905044c4675f051158827b6d6bac5b2abc7e0
MYGelo <112314820+MYGelo@users.noreply.github.com>
2022-12-09 01:58:50 +0000

    Deploying to gh-pages from @ MYGelo/goit-js-hw-09@e953db2fcab1ef687022f40424c87f79d295ed5a 🚀

--- 02-timer.3f19d9ba.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=new Date;console.dir(n)}();
-//# sourceMappingURL=02-timer.3f19d9ba.js.map
--- 02-timer.7d7f892d.js
+++
@@ -1,2 +0,0 @@
-const e=new Date;console.dir(e);
-//# sourceMappingURL=02-timer.7d7f892d.js.map
