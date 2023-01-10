1f20a60d221d7208cff505e3dbe70c1cb314e32e
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-01-10 12:16:47 +0200

    fix delay +=

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -25,11 +25,12 @@ buttonSubmit.addEventListener('click', onClick);
 
 async function onClick(e) {
   e.preventDefault();
+
   let firstDelay = Number(refs.delay.value);
   let delayStep = Number(refs.step.value);
+
   for (let i = 0; i < refs.amount.value; i++) {
-    let delay = i === 0 ? firstDelay : delayStep;
-    await createPromise(1 + i, delay)
+    await createPromise(i + 1, firstDelay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
@@ -40,5 +41,6 @@ async function onClick(e) {
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
+    firstDelay += delayStep;
   }
 }
